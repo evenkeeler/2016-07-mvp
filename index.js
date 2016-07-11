92bd9bb38a80c124ab50945704f85f94c55553a4
evenkeeler <connor.d.campbell@gmail.com>
2016-07-11 18:48:07 -0400

    more tests

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var app = require('./server/server.js');
  var port = process.env.PORT || 3000;
 
 
-app.get('/', function(req, res){
-  res.render('./client/index.html');
-});
+// app.get('/', function(req, res){
+//   res.render('./client/index.html');
+// });
+
+app.use('/', express.static('client'));
+
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
