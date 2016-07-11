1df5466314eabbc481540876fb2188ece2b4362e
evenkeeler <connor.d.campbell@gmail.com>
2016-07-11 18:17:49 -0400

    ladddida

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var app = require('./server.js');
+var app = require('server/server.js');
 
 
  var port = process.env.PORT || 3000;
 
 app.listen(port);
+
+app.get('/', function(req, res){
+  res.render('client/index.js');
+});
 
 console.log('Server now listening on port ' + port);
 
