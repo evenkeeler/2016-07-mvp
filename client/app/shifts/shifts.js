b2d87306061396e1821b21989685e5a69a075b31
evenkeeler <connor.d.campbell@gmail.com>
2016-07-12 07:53:58 -0400

    more changes

--- client/app/shifts/shifts.js
+++ client/app/shifts/shifts.js
@@ -1,6 +1,6 @@
 angular.model('quickShift.shifts', [])
 .controller('ShiftController', function($scope, Shifts){
-  this.shifts = Shifts.shifts;
+  $scope.shifts = Shifts.shifts;
 })
 .controller("ShiftDirector", function($scope, $location){
   this.render = function(){
