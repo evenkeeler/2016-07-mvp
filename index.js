4178be3ed566dcd6282ca04da4fd951b5c310c87
evenkeeler <connor.d.campbell@gmail.com>
2016-07-12 10:53:27 -0400

    still struggling

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-app.get('/shifts', function(req, res){
-  res.end('blah');
-})
-
-app.get
-
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
